d0deba02f3e2cee1d52e2f34b01cd7884f4cdaaa
tafrij <mamunamri97@gmail.com>
2018-11-22 17:23:57 +0700

    git add *

---
+++ toko-online/app/Http/Controllers/ProductController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+  public function showAll() {
+  // kode logika untuk mengambil dan menampilkan semua data
+  }
+  public function saveNew(Request $request){
+    // kode logika untuk menyimpan product baru
+  }
+}
